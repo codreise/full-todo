a5242ed57b5a60b259d26a6e4e5b9b35c07b519e
kirichenkod092@gmail.com <kirichenkod092@gmail.com>
2023-04-01 23:28:43 +0300

    test

--- server/index.js
+++ server/index.js
@@ -1,8 +1,28 @@
 const http = require('http');
 const app = require('./app');
+const { Server } = require("socket.io");
+
+const cors = {
+    origin: '*'
+}
+
 
 const PORT = process.env.PORT || 5000;
 const server = http.createServer(app);
+
+const io = new Server(server, cors);
+
+io.on("connection", (socket) => {
+  console.log('CONNECTION!!!!');
+
+  setTimeout(() => {
+    io.emit('NEW_NOTIFICATION', {notification: 'Something new happened'})
+  }, 5000);
+
+  socket.on('disconnect', (reason) => {
+    console.log(reason);
+  })
+});
 
 server.listen(PORT, () => {
     console.log(`App started on port ${PORT}`);
